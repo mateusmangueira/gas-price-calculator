8ed80e0e3d0edf63de6c132e0935e6afed732718
Mateus Pinto Mangueira <mateus.mangueira@ab-inbev.com>
2024-06-05 07:48:00 -0300

    style: add components and style in the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
 import "./App.css";
+import logoImg from "./assets/logo.png";
 
 function App() {
   return (
-    <>
-      <h1 className="header"> Under construction... </h1>
-    </>
+    <div>
+      <main className="container">
+        <img src={logoImg} alt="logo-calculator" className="logo-img" />
+        <h1 className="title">Qual melhor escolha?</h1>
+
+        <form action="">
+          <label>Gasolina (preço por litro):</label>
+
+          <input
+            className="input"
+            type="number"
+            placeholder="R$ 5,60"
+            min="1"
+            step="0.01"
+            required
+          />
+
+          <label>Álcool (preço por litro):</label>
+
+          <input
+            className="input"
+            type="number"
+            placeholder="R$ 3,99"
+            min="1"
+            step="0.01"
+            required
+          />
+
+          <input type="submit" value="Calcular" className="button" />
+        </form>
+      </main>
+    </div>
   );
 }
 
